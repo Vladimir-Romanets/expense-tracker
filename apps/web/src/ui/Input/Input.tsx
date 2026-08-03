9e64db1c5a13f868023371d7ab0283636feeb8d8
Vladimir Romanets <rva.wsd@gmail.com>
2026-08-03 19:39:47 +0200

    FE - Add Input element

---
+++ apps/web/src/ui/Input/Input.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { cn } from '@/utils/cn'
+import { inputVariants } from './input.variants'
+
+export interface InputProps extends Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  'size'
+> {
+  label?: string
+  error?: string
+  wrapperClassName?: string
+  ref?: React.Ref<HTMLInputElement>
+}
+
+export const Input = ({
+  className,
+  wrapperClassName,
+  label,
+  error,
+  id,
+  ref,
+  ...props
+}: InputProps) => {
+  const generatedId = id || props.name
+  const errorId = generatedId ? `${generatedId}-error` : undefined
+
+  return (
+    <div className={cn('flex w-full flex-col gap-1.5', wrapperClassName)}>
+      {label && (
+        <label
+          htmlFor={generatedId}
+          className="text-foreground text-sm font-medium"
+        >
+          {label}
+        </label>
+      )}
+      <div className="relative">
+        <input
+          id={generatedId}
+          ref={ref}
+          aria-invalid={!!error}
+          aria-describedby={error ? errorId : undefined}
+          className={cn(inputVariants({ isError: !!error }), className)}
+          {...props}
+        />
+      </div>
+      {error && (
+        <span
+          id={errorId}
+          className="text-xs text-red-500"
+        >
+          {error}
+        </span>
+      )}
+    </div>
+  )
+}
+
+Input.displayName = 'Input'
---
+++ apps/web/src/ui/Input/RHFInput.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import {
+  Controller,
+  type Control,
+  type FieldValues,
+  type Path,
+} from 'react-hook-form'
+import { Input, type InputProps } from './Input'
+
+export interface RHFInputProps<T extends FieldValues> extends Omit<
+  InputProps,
+  'name'
+> {
+  name: Path<T>
+  control: Control<T>
+}
+
+export const RHFInput = <T extends FieldValues>({
+  name,
+  control,
+  ...rest
+}: RHFInputProps<T>) => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState }) => (
+        <Input
+          {...rest}
+          {...field}
+          error={fieldState.error?.message}
+        />
+      )}
+    />
+  )
+}
+
+Input.displayName = 'RHFInput'
