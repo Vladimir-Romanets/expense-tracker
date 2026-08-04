66851714e4f59d5cbe019ded336a0373c9fde3bc
Vladimir Romanets <rva.wsd@gmail.com>
2026-08-04 17:59:14 +0200

    FE - Updated PageTitle to work dynamically

--- apps/web/src/app/(protected)/overview/page.tsx
+++ apps/web/src/app/(protected)/overview/page.tsx
@@ -1,9 +1,11 @@
 import { Typography } from '@/ui'
 
-export default function ProtectedDashboardPage() {
+const ProtectedDashboardPage = () => {
   return (
     <div className="rounded-2xl border border-surface-border bg-surface-card p-8 shadow-xs">
       <Typography variant="p">Protected area content placeholder</Typography>
     </div>
   )
 }
+
+export default ProtectedDashboardPage
