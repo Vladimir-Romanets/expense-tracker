934ddd6a4e17e45427d514dab06da450060fc578
Vladimir <rva.wsd@gmail.com>
2026-08-07 13:29:19 +0200

    Feature/api add function cloudflare file uploading (#71)
    * API - Add function Cloudflare file uploading
    
    * API - Add function Cloudflare file uploading

--- apps/api/src/controllers/uploads.ts
+++ apps/api/src/controllers/uploads.ts
@@ -1,25 +1,13 @@
 import { Response } from 'express'
-import { PutObjectCommand } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { randomUUID } from 'crypto'
-import { r2Client, R2_BUCKET } from '@helpers/utils/r2'
 import { asyncHandler } from '@helpers/errors/asyncHandler'
 import { AuthRequest } from '@middleware/authenticate'
 import { PresignedUploadDto } from '@validators/uploads'
+import { uploadsService } from '@services'
 
 export const uploadFile = asyncHandler(async (req: AuthRequest, res: Response) => {
   const { contentType, fileSize } = req.body as PresignedUploadDto
-  const { userId } = req
-  const ext = contentType.split('/')[1]
-  const imageKey = `receipts/${userId}/${randomUUID()}.${ext}`
+  const userId = req.userId as number
 
-  const command = new PutObjectCommand({
-    Bucket: R2_BUCKET,
-    Key: imageKey,
-    ContentType: contentType,
-    ContentLength: fileSize,
-  })
-
-  const uploadUrl = await getSignedUrl(r2Client, command, { expiresIn: 300 })
-  res.status(200).json({ uploadUrl, imageKey })
+  const response = await uploadsService.uploadFile({ contentType, fileSize, userId })
+  res.json(response)
 })
