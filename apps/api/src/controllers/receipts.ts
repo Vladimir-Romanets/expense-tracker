bd3c211564ec365e133200f0d989a7aef164ace3
Vladimir <rva.wsd@gmail.com>
2026-08-12 12:58:05 +0200

    API - Added endpoint to retrive certain receipt (#95)

--- apps/api/src/controllers/receipts.ts
+++ apps/api/src/controllers/receipts.ts
@@ -1,15 +1,23 @@
 import { Response } from 'express'
 import { asyncHandler } from '@helpers/errors/asyncHandler'
 import { receiptsService } from '@services'
-import { AuthRequest } from '@middleware/authenticate'
-import { ValidatedRequest } from '../types/validator'
-import { DeleteReceiptDto } from '@validators/receipts'
+import type { ValidatedRequest } from '../types/validator'
+import type { ParamsIdReceiptDto } from '@validators/receipts'
+import type { AuthRequest } from '@middleware/authenticate'
 
 export const getAll = asyncHandler(async (req: AuthRequest, res: Response) => {
   const response = await receiptsService.getAll(req.query, req.userId as number)
 
   res.status(200).json(response)
 })
+
+export const getById = asyncHandler(
+  async (req: ValidatedRequest<ParamsIdReceiptDto> & AuthRequest, res: Response) => {
+    const response = await receiptsService.getById(req.params.id, req.userId as number)
+
+    res.status(200).json(response)
+  },
+)
 
 export const create = asyncHandler(async (req: AuthRequest, res: Response) => {
   const response = await receiptsService.addFullReceiptData(req.body, req.userId as number)
@@ -18,7 +26,7 @@ export const create = asyncHandler(async (req: AuthRequest, res: Response) => {
 })
 
 export const remove = asyncHandler(
-  async (req: ValidatedRequest<DeleteReceiptDto> & AuthRequest, res: Response) => {
+  async (req: ValidatedRequest<ParamsIdReceiptDto> & AuthRequest, res: Response) => {
     await receiptsService.remove(req.params.id, req.userId as number)
 
     res.status(204).send()
