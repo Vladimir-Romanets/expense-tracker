2c58da638137c5b75795b1d45938c1bcdd309b2a
Vladimir <rva.wsd@gmail.com>
2026-09-22 16:44:19 +0200

    Revert "feat: Add capture attribute to file upload field (#150)"
    This reverts commit b8a2732a9a39fe2a337459d482971be50cfcc03c.

--- apps/web/src/features/receipts/components/forms/ReceiptForm.tsx
+++ apps/web/src/features/receipts/components/forms/ReceiptForm.tsx
@@ -68,7 +68,6 @@ export function ReceiptForm({
             label="Upload or drag and-drop scan/photo of receipt."
             className="h-96"
             accept="image/jpeg,image/png,image/webp"
-            capture="environment"
             disabled={isPending}
           />
           <Button
